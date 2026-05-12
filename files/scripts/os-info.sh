ba841ed4d04b2c276a5ef6eb4c767aa69b9f9ca8
Sylvie00 <taylorkellogg540@gmail.com>
2026-05-12 14:19:52 -0500

    Switch base to bazzite and add os-info

---
+++ files/scripts/os-info.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -ouex pipefail
+
+IMAGE_NAME="goobernix"
+IMAGE_PRETTY_NAME="goobernix"
+VERSION_ID=$(grep -Po '(?<=^VERSION_ID=).*' /usr/lib/os-release | tr -d '"')
+HOME_URL="https://github.com/Sylvie00/goobernix"
+
+sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME=\"${IMAGE_PRETTY_NAME} ${VERSION_ID}\"/" /usr/lib/os-release
+sed -i "s/^NAME=.*/NAME=\"$IMAGE_PRETTY_NAME\"/" /usr/lib/os-release
+sed -i "s/^ID=fedora/ID=${IMAGE_NAME}/" /usr/lib/os-release
+sed -i "s/^ID_LIKE=.*/ID_LIKE=\"fedora\"/" /usr/lib/os-release
+sed -i "s|^HOME_URL=.*|HOME_URL=\"$HOME_URL\"|" /usr/lib/os-release
+sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME=\"${IMAGE_PRETTY_NAME,,}\"/" /usr/lib/os-release
