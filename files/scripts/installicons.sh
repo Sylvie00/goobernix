5e44aff3aea515d7da521a152feebc1e4a2e8463
Sylvie00 <taylorkellogg540@gmail.com>
2026-05-12 16:18:34 -0500

    fix icons script

--- files/scripts/installicons.sh
+++ files/scripts/installicons.sh
@@ -4,6 +4,6 @@ set -ouex pipefail
 
 mkdir /tmp/icons
 git clone --depth 1 https://github.com/PonyRoleplayer/WinXP-icon-theme.git /tmp/icons
-cp /tmp/icons/winxp /usr/share/icons/winxp
+cp -r /tmp/icons/winxp /usr/share/icons/winxp
 
 echo yayyyyy
